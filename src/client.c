ab1bbc59d4936155a2535e66414782045a38f7b0
aoshi2025s <aoshi2025s@gmail.com>
2024-07-28 00:41:30 +0900

    mandatory ok

--- src/client.c
+++ src/client.c
@@ -5,9 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/15 05:34:40 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/05/15 05:34:42 by yoaoki           ###   ########.fr       */
+/*   Created: 2024/07/28 00:33:57 by yoaoki            #+#    #+#             */
+/*   Updated: 2024/07/28 00:37:12 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+void	send_bits(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (1 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+	{
+		printf("Error: wrong format.\n");
+		printf("Try: ./client [PID] [MESSAGE]\n");
+		return (1);
+	}
+	pid = atoi(argv[1]);
+	while (*argv[2])
+	{
+		send_bits(pid, *argv[2]);
+		argv[2]++;
+	}
+	send_bits(pid, '\n');
+	return (0);
+}
--- src/server.c
+++ src/server.c
@@ -6,8 +6,46 @@
 /*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 05:34:16 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/05/15 05:34:18 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/07/28 00:29:44 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+void	ft_handler(int signal)
+{
+	static int	bit;
+	static int	i;
+
+	if (signal == SIGUSR1)
+		i |= (1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 1)
+	{
+		printf("Error: wrong format.\n");
+		printf("Try: ./server\n");
+		return (1);
+	}
+	printf("[PID]: %d\n", getpid());
+	printf("Waiting for a message...\n");
+	while (1)
+	{
+		signal(SIGUSR1, ft_handler);
+		signal(SIGUSR2, ft_handler);
+		pause();
+	}
+	return (0);
+}
