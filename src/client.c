6cef62f1090f870f61c222a41b7a1e8dbfaf41bb
aoshi2025s <aoshi2025s@gmail.com>
2025-04-18 07:04:42 +0900

    fix norminette formatter

--- src/client.c
+++ src/client.c
@@ -1,17 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 06:54:30 by yoaoki            #+#    #+#             */
+/*   Updated: 2025/04/18 07:01:25 by yoaoki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
 // use async-signal safe function (do not use printf)
-void ft_confirm(int signal, siginfo_t *info, void *s) {
+void	ft_confirm(int signal, siginfo_t *info, void *s)
+{
 	(void)signal;
 	(void)info;
 	(void)s;
-	write(1, "Message received!\n", 18); 
+	write(1, "Message received!\n", 18);
 }
 
-void ft_send_bits(int pid, char i) {
-	int bit;
+void	ft_send_bits(int pid, char i)
+{
+	int	bit;
+
 	bit = 0;
-	while (bit < 8) {
+	while (bit < 8)
+	{
 		if ((i & (1 << bit)) != 0)
 			kill(pid, SIGUSR1);
 		else
@@ -21,29 +37,40 @@ void ft_send_bits(int pid, char i) {
 	}
 }
 
-int main(int argc, char **argv) {
-	int pid;
-	struct sigaction sa;
+int	ft_get_pid(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: ./client [PID] [Message]\n");
+		exit(1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid <= 0 || kill(pid, 0) == -1)
+	{
+		ft_printf("Error: PID not valid\n");
+		exit(1);
+	}
+	return (pid);
+}
+
+int	main(int argc, char **argv)
+{
+	int					pid;
+	struct sigaction	sa;
 
 	sa.sa_sigaction = ft_confirm;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR2, &sa, NULL) == -1) {
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
 		ft_printf("Error client sigaction\n");
 		exit(1);
 	}
-
-	if (argc != 3) {
-		ft_printf("Usage: ./client [PID] [Message]\n");
-		return (1);
-	}
-
-	pid = ft_atoi(argv[1]);
-	if (pid <= 0 || kill(pid, 0) == -1) {
-		ft_printf("Error: PID not valid\n");
-		exit(1);
-	}
-	while (*argv[2]) {
+	pid = ft_get_pid(argc, argv);
+	while (*argv[2])
+	{
 		ft_send_bits(pid, *argv[2]);
 		argv[2]++;
 	}
--- src/server.c
+++ src/server.c
@@ -1,18 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 06:54:47 by yoaoki            #+#    #+#             */
+/*   Updated: 2025/04/18 06:56:51 by yoaoki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-void ft_handler(int signal, siginfo_t *info, void *s) {
-	static int bit = 0;
-	static int i = 0;
+void	ft_handler(int signal, siginfo_t *info, void *s)
+{
+	static int	bit = 0;
+	static int	i = 0;
 
 	(void)s;
 	if (signal == SIGUSR1)
 		i |= (1 << bit);
 	bit++;
-	if (bit == 8) {
-		if (i == '\0') {
+	if (bit == 8)
+	{
+		if (i == '\0')
+		{
 			kill(info->si_pid, SIGUSR2);
 			write(1, "\n", 1);
-		} else {
+		}
+		else
+		{
 			write(1, &i, 1);
 		}
 		bit = 0;
@@ -20,24 +37,23 @@ void ft_handler(int signal, siginfo_t *info, void *s) {
 	}
 }
 
-int main(void) {
-	int pid;
-	struct sigaction sa;
+int	main(void)
+{
+	int					pid;
+	struct sigaction	sa;
 
 	pid = getpid();
 	ft_printf("[PID]: %d\n", pid);
 	ft_printf("Waiting for a message...\n");
-
 	sa.sa_sigaction = ft_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
-	
 	if (sigaction(SIGUSR1, &sa, NULL) == -1
-		|| sigaction(SIGUSR2, &sa, NULL) == -1) {
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
 		ft_printf("Error: server sigaction\n");
 	}
-
 	while (1)
 		pause();
-	return 0;
+	return (0);
 }
