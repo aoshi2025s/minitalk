b51c7b1d4ce24dee125b8b0a2c488b5e79edd1a3
aoshi2025s <aoshi2025s@gmail.com>
2025-04-18 05:19:28 +0900

    use sigaction

--- src/client.c
+++ src/client.c
@@ -1,49 +1,54 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/28 00:33:57 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/08/02 19:30:00 by yoaoki           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
 
-#include "minitalk.h"
+void ft_confirm(int signal, siginfo_t *info, void *s) {
+	(void)info;
+	(void)s;
+	(void)signal;
+	printf("Message received!\n");
+}
 
-void	send_bits(int pid, char c)
-{
-	int	bit;
+void ft_send_bits(int pid, char i) {
+	int bit;
 
 	bit = 0;
-	while (bit < 8)
-	{
-		if ((c & (1 << bit)) != 0)
+	while (bit < 8) {
+		if ((i & (1 << bit)) != 0)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(100);
+		// これもusleep(1)でもあれば今の所動作する。
+		// 割り込み中にもう一方のシグナルが発生するとバグる(文字化け？)
+		usleep(100); // レースコンディション対策って何？
 		bit++;
 	}
 }
 
-int	main(int argc, char **argv)
-{
-	int	pid;
+int main(int argc, char **argv) {
+	int pid;
+	struct sigaction sa;
 
-	if (argc != 3)
-	{
-		ft_printf("Error: wrong format.\n");
-		ft_printf("Try: ./client [PID] [MESSAGE]\n");
+	sa.sa_sigaction = ft_confirm;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR2, &sa, NULL);
+
+	if (argc != 3) {
+		printf("Usage: ./client [PID] [Message]\n");
 		return (1);
 	}
-	pid = ft_atoi(argv[1]);
-	while (*argv[2])
-	{
-		send_bits(pid, *argv[2]);
+
+	pid = atoi(argv[1]);
+	if (pid <= 0 || kill(pid, 0) == -1) {
+		printf("Error: PID not valid\n");
+		exit(1);
+	}
+	while (*argv[2]) {
+		ft_send_bits(pid, *argv[2]);
 		argv[2]++;
 	}
-	send_bits(pid, '\n');
+	ft_send_bits(pid, '\0');
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -1,49 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/15 05:34:16 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/08/02 19:30:39 by yoaoki           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 
-#include "minitalk.h"
+// TODO: 非同期安全な関数とは？
 
-void	ft_handler(int signal)
-{
-	static int	bit;
-	static int	i;
+void ft_handler(int signal, siginfo_t *info, void *s) {
+	static int bit = 0;
+	static int i = 0;
 
+	(void)s;
 	if (signal == SIGUSR1)
 		i |= (1 << bit);
 	bit++;
-	if (bit == 8)
-	{
-		ft_printf("%c", i);
+	if (bit == 8) {
+		if (i == '\0') {
+			kill(info->si_pid, SIGUSR2);
+			write(1, "\n", 1);
+		} else {
+			write(1, &i, 1);
+		}
 		bit = 0;
 		i = 0;
 	}
 }
 
-int	main(int argc, char **argv)
-{
-	(void)argv;
-	if (argc != 1)
-	{
-		ft_printf("Error: wrong format.\n");
-		ft_printf("Try: ./server\n");
-		return (1);
-	}
-	ft_printf("[PID]: %d\n", getpid());
-	ft_printf("Waiting for a message...\n");
+int main(void) {
+	int pid;
+	struct sigaction sa;
+
+	pid = getpid();
+	printf("[PID]: %d\n", pid);
+	printf("Waiting for a message...\n");
+
+	sa.sa_sigaction = ft_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO; // sa.sa_flags = 0じゃだめ？
+	
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+
 	while (1)
-	{
-		signal(SIGUSR1, ft_handler);
-		signal(SIGUSR2, ft_handler);
 		pause();
-	}
-	return (0);
+	return 0;
 }
